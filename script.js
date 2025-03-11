c55fe224cbfab3dabae8dbad6d482e443334118f
anajuliaportfolio <anajuliaportifolio@gmail.com>
2025-03-11 13:00:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
   })(jQuery);
 
   // input text for typing animation
-  $("#holder").writeText("WEB DESIGNER + FRONT-END DEVELOPER");
+  $("#holder").writeText("UI DESIGNER | SOCIAL MEDIA MANAGER");
 
   // initialize wow.js
   new WOW().init();
